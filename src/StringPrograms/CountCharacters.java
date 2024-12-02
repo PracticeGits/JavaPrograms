05c12ab9e6fe6e4f9998b2d81c206c16dffe2fb7
praveen <163524331+PracticeGits@users.noreply.github.com>
2024-12-02 10:26:13 +0530

    Update CountCharacters.java

--- src/StringPrograms/CountCharacters.java
+++ src/StringPrograms/CountCharacters.java
@@ -3,7 +3,7 @@ package StringPrograms;
 public class CountCharacters{
 
     public static  void main(String[] args) {
-        //login and merge
+        //login and merge.
         String s ="Welcome Home buddy";
         int count = 0;
         for(int i=0;i<=s.length()-1;i++){
