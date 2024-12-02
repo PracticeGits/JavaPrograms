8dad244c18cc322054e9c5ab399a2c46aad2b786
praveen <praveenraj9747@gmail.com>
2024-12-02 10:30:01 +0530

    stashed

--- src/StringPrograms/CountCharacters.java
+++ src/StringPrograms/CountCharacters.java
@@ -3,7 +3,11 @@ package StringPrograms;
 public class CountCharacters{
 
     public static  void main(String[] args) {
+<<<<<<< Updated upstream
         //login and merge.
+=======
+
+>>>>>>> Stashed changes
         String s ="Welcome Home buddy";
         int count = 0;
         for(int i=0;i<=s.length()-1;i++){
